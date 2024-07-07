719b9bec360149fe036f3bd3d4a4494bc7ed6f00
nahid <nxnahidxyz@gmail.com>
2024-07-07 11:51:47 +0600

    price tag color changed

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import img from '../../assets/image.png'
+import { TbCurrencyTaka } from 'react-icons/tb'
 
 const Card = () => {
   return (
@@ -8,8 +9,8 @@ const Card = () => {
       <img className=' transition-all hover:scale-125 h-[144px] sm:h-[300px] w-full object-cover rounded-2xl' src={img} alt="img" />
       <div className='absolute hover:h-[80px] transition-all flex flex-col bottom-0 left-[50%] translate-x-[-50%] h-[60px] sm:h-[70px] w-[80%] bg-slate-300 rounded-b-xl rounded-t-2xl overflow-hidden'>
         <div className=' flex sm:gap-4 justify-around sm:justify-center sm:text-xl h-[30px]'>
-          <h2 className=' line-through text-rose-600'>$400</h2>
-          <h2 className=' text-stone-700 sm:font-medium'>$350</h2>
+          <h2 className=' line-through text-stone-500 flex items-center'><TbCurrencyTaka />400</h2>
+          <h2 className=' text-blue-500 sm:font-medium flex items-center'><TbCurrencyTaka />350</h2>
         </div>
         <button className=' flex-1 sm:text-xl text-stone-200 sm:font-medium bg-green-400'>Add to Cart</button>
       </div>
