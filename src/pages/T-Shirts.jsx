ae2d13fad0e25392b2b4bbf7e442c190c27dd726
nahid <nxnahidxyz@gmail.com>
2024-07-07 11:32:40 +0600

    nothing changed

--- src/pages/T-Shirts.jsx
+++ src/pages/T-Shirts.jsx
@@ -4,9 +4,9 @@ import Card from '../components/Card/Card'
 const TShirts = () => {
   return (
     <div>
-      <h2 className=' text-3xl text-center text-stone-500 my-5 md:my-10 cursor-default'>T-Shirts</h2>
+      <h2 className=' text-3xl text-center text-stone-500 my-5 md:mt-10 cursor-default'>T-Shirts</h2>
 
-      <div className="border-b-2 py-5 md:py-20 border-blue-300 all-cards flex flex-wrap justify-evenly sm:gap-y-16 gap-4">
+      <div className="border-b-2 py-5 md:py-10 border-blue-300 all-cards flex flex-wrap justify-evenly sm:gap-y-16 gap-4">
         <Card />
         <Card />
         <Card />
